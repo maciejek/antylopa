45ecc52e917396146bad28ac4b7bd7bd47617050
Maciek R. <radko.m@hotmail.com>
2013-04-15 21:56:48 +0200

    cos

--- app/models/plik.rb
+++ app/models/plik.rb
@@ -1,16 +1,9 @@
 class Plik < ActiveRecord::Base
-	#belongs_to:user_email, :class_name => 'Uzytkownik'
+	belongs_to:userID, :class_name => 'Uzytkownik'
 	has_many:Wersja
 	has_many:Komentarz
 	
-	attr_accessible :idPlik, :user_email, :projektID, :publiczny, :tylkoOdczyt, :sciezka, :created_at, :updated_at
+	attr_accessible :idPlik, :userID, :projektID, :publiczny, :tylkoOdczyt, :sciezka, :created_at, :updated_at
 	mount_uploader :sciezka, PlikUploader
 	
-	def IdPlik
-	idPlik
-	end
-	
-	def setUser(email)
-	user_email = email
-	end
 end
